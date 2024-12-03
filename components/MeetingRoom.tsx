9475d4459c905d21313b5da60c6645236f799ded
Mayuresh <mayureshprabhu29@gmail.com>
2024-12-03 16:57:02 +0530

    Meetin Room Error fixed

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -9,7 +9,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
